35b658c5d18099eda2bc6740079ff4da81b2b48d
朱银权 <319246676@qq.com>
2024-05-22 13:40:56 +0800

    修改后的提交

--- yingfu/routing.py
+++ yingfu/routing.py
@@ -4,8 +4,7 @@ from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
-    re_path(r'ws/private/(?P<room_name>\w+)/$', PrivateChatConsumer.as_asgi()),
-    re_path(r'ws/p-p/(?P<user_name>\w+)/$', PrivateChatConsumer2.as_asgi()),
+    
 ]
 
 
